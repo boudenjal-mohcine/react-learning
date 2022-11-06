15bb7d28f72ee11336e000512a4ed8c7c6fa5436
boudenjal-mohcine <mohcineboudenjal@gmail.com>
2022-11-06 14:02:21 +0100

    Add categories filter and update ShoppingList component

--- src/components/App.js
+++ src/components/App.js
@@ -3,19 +3,32 @@ import Banner from './Banner';
 import Cart from './Cart';
 import ShoppingList from './ShoppingList';
 import Footer from './Footer';
-import '../styles/Layout.css'
+import '../styles/Layout.css';
+import {useState} from 'react';
 
 function App() {
+
+  const [cart, updateCart] = useState([])
+
   return (
     <div>
 		<Banner />
 			<div className='lmj-layout-inner'>
-				<Cart />
-				<ShoppingList />
-			</div>
+          <Cart cart={cart} updateCart={updateCart} />
+          <ShoppingList cart={cart} updateCart={updateCart} />
+		  </div>
 			<Footer />
 		</div>
   );
 }
 
 export default App;
+
+
+/***
+ * 
+ * Pour utiliser un même état entre plusieurs composants, il faut :
+ * faire remonter l'état dans le composant parent commun le plus proche ;
+ * puis faire descendre la variable d'état et la fonction pour mettre à jour cet état dans des props.
+ * 
+ ***/
