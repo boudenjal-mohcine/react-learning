7dc28cc03c660b3d05c2ba24244c4a9168a72f5e
boudenjal-mohcine <mohcineboudenjal@gmail.com>
2022-11-06 11:40:49 +0100

    add event of open and close cart

--- src/components/PlantItem.js
+++ src/components/PlantItem.js
@@ -1,13 +1,18 @@
 import CareScale from './CareScale'
 import '../styles/PlantItem.css'
 
-function handleClick() {
-    alert('✨ Ceci est un clic ✨')
+// function handleClick(plantName) {
+//     alert(`Vous voulez acheter 1 ${plantName} ? Très bon choix 🌱✨`)
+// }
+
+
+function handleClick(e) {
+    console.log('✨ Ceci est mon event :', e)
 }
 
 function PlantItem({ id, cover, name, water, light }) {
 	return (
-		<li key={id} className='lmj-plant-item' onClick={handleClick(name)}>
+		<li key={id} className='lmj-plant-item' onClick={() => handleClick(name)}>
 			<img className='lmj-plant-item-cover' src={cover} alt={`${name} cover`} />
 			{name}
 			<div>
@@ -19,3 +24,13 @@ function PlantItem({ id, cover, name, water, light }) {
 }
 
 export default PlantItem
+
+// React event syntax
+
+// l'événement s'écrit dans une balise encamelCase;
+
+// vous déclarez l'événement à capter, et lui passez entre accolades la fonction à appeler ;
+
+// contrairement au JS, dans la quasi totalité des cas, vous n'avez pas besoin d'utiliser addEventListener
+
+// click event : onClick={() => handleClick(name)}
